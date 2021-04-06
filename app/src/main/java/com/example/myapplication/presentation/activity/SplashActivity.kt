6cb8362781a66647efd36c91e143d673d377da56
Alejandro Garcia <dagarcia@novasolutionsystems.com>
2021-04-06 16:57:51 -0500

    Project upload files

---
+++ app/src/main/java/com/example/myapplication/presentation/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
